cf33fa0f37607e936ff3d20ffd2c618b52daf728
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-01-25 17:46:37 +0200

    Add serializers

--- eea/volto/slate/interfaces.py
+++ eea/volto/slate/interfaces.py
@@ -1,7 +1,16 @@
 """Module where all interfaces, events and exceptions live."""
 
+from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class IEeaVoltoSlateLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class ISlateNodeSerializeTransform(Interface):
+    """Transformers for slate nodes serialization (publish to frontend)"""
+
+
+class ISlateNodeDeserializeTransform(Interface):
+    """Transformer on slate nodes deserialization (save to backend)"""
